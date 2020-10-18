6bc67f1391283b11d94788d16bab73d92353fb05
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-18 11:41:08 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,17 @@
 fun main(){
+    printA(5)
 
+    val array = listOf(1,2,3,4,5,6,7,8,9,10)
+
+    println(array)
+    println(array.map{it*it})
+
+    //x-e^(-x) = 0
+    //4x+sin(x) = 0
 }
+
+fun add(a: Int, b: Int):Int{
+    return a+b
+}
+
+fun printA(a: Int) = println("Wartość wynosi ${a}")
