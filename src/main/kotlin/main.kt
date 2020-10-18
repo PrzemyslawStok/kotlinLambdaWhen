1b837310382e8c81e8786f053363d395a8b3eecb
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-18 11:44:42 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,13 @@
 fun main(){
     printA(5)
 
+    //x-e^(-x) = 0
+    //4x+sin(x) = 0
+
     val array = listOf(1,2,3,4,5,6,7,8,9,10)
 
     println(array)
     println(array.map{it*it})
-
-    //x-e^(-x) = 0
-    //4x+sin(x) = 0
 }
 
 fun add(a: Int, b: Int):Int{
