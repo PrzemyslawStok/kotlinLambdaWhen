74a7e70a797af92134489484e73660617102d1af
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-18 12:14:12 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import java.lang.Math.exp
+
 fun main(){
     printA(5)
 
@@ -5,11 +7,17 @@ fun main(){
     //4x+sin(x) = 0
 
     val array = listOf(1,2,3,4,5,6,7,8,9,10)
-    val square = {x:Int->x*x}
+    var square:(Int)->Int = {x:Int->x*x}
+
     val add = {a:Int,b:Int->a+b}
+    val sub = {a:Int,b:Int->a-b}
+    val equation = {x:Double->x-exp(-x)}
+
+    val div:(Int,Int)->Double = {x:Int,y:Int->x/y.toDouble()}
 
     println("kwadrat wynosi: ${square(5)}")
     println("suma wynosi ${add(2,5)}")
+    println("iloraz wynosi ${div(2,5)}")
 
     println(array)
     println(array.map(square))
@@ -22,3 +30,7 @@ fun add1(a: Int, b: Int):Int{
 }
 
 fun printA(a: Int) = println("Wartość wynosi ${a}")
+
+fun math(a:Int, b:Int):Double{
+    return 0.0
+}
